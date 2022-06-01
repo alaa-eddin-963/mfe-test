bbd903a546b8d2c2f9ba4e7c02502a94d659ae5d
alaa eddin <alaaeddin785@gmail.com>
2022-06-01 15:28:12 +0300

    fix contaienr product

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -5,12 +5,12 @@ const packageJson = require("../package.json")
 const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: "production",
+    output: {
+        filename: "[name][contenthash].js"
+    },
     plugins: [
         new ModuleFederationPlugin({
             name: "container",
-            output: {
-                filename: "[name][contenthash].js"
-            },
             remotes: {
                 marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
