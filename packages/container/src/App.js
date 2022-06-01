4e729cc1dd0eb7a523288c48c3a521a99d8fb49a
alaa eddin <alaaeddin785@gmail.com>
2022-06-01 15:10:17 +0300

    init commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+const App = () => (
+    <>
+        <h1>Hello world</h1>
+        <MarketingApp />
+    </>
+)
+export default App
